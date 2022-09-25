854ff60d58b0e706eeb6558476fe77241391c6e8
Prash <prash315@users.noreply.github.com>
2022-09-25 16:02:13 -0400

    Update ec2_module.tf

--- ec2_module/ec2_module.tf
+++ ec2_module/ec2_module.tf
@@ -1,20 +1,12 @@
-variable "ami_id" {
-  type = string
-}
-
 module "shared_vars" {
   source = "../shared_vars"
 }
 
-variable "sg_id" {
-  type = string
-}
-
 resource "aws_instance" "terraform_ec2_instance" {
-  ami           = "${var.amiid}"
+  ami           = var.ami_id
   instance_type = "t2.micro"
   key_name = "pp_terraform_course"
-  vpc_security_group_ids = ["${var.sg_id}"] # replaced previous by variable "sg_id"
+  vpc_security_group_ids = var.sg_id # replaced previous by variable "sg_id"
   
   tags = {
     Name = "EC2_Name_Instance_${module.shared_vars.env_suffix}"
