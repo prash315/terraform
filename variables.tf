a456a9b54086caabd3a01fed1d4f2016a3ca58c9
Prash <prash315@users.noreply.github.com>
2022-09-25 18:54:57 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,16 @@
-variable "ami_id" {}
-variable "region" {}
-variable "sg_id" {}
-variable "vpc_id" {}
+variable "ami_id" {
+  type = string
+  default = ""
+}
+variable "region" {
+  type = string
+  default = ""
+}
+variable "sg_id" {
+  type = string
+  default = ""
+}
+variable "vpc_id" {
+  type = string
+  default = ""
+}
