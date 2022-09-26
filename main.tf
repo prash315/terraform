b8c4ba1f34aeacf207dae444c837a4104698e4fd
Prash <prash315@users.noreply.github.com>
2022-09-25 21:55:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ provider "aws" {
   }
 }
 
+variable ami_id = {}
+variable vpc_id = {}
+
 module "sg_module" {
   source = "./sg_module"
-  variable vpc_id = {}
-
 }
 
 module "ec2_module_1" {
   source = "./ec2_module"
-  variable ami_id = {}
 }
 
 output "envSpecificOutputVariable" {
