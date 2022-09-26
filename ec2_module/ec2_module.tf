fdda6d106407de1bc228cb04d602fd3d4466bf23
Prash <prash315@users.noreply.github.com>
2022-09-25 22:57:27 -0400

    Update ec2_module.tf

--- ec2_module/ec2_module.tf
+++ ec2_module/ec2_module.tf
@@ -6,7 +6,7 @@ module "sg_module" {
   ami           = var.ami_id
   instance_type = "t2.micro"
   key_name = "pp_terraform_course"
-  vpc_security_group_ids = module.sg_module.sg_output # replaced previous by variable "sg_id"
+  vpc_security_group_ids = [module.sg_module.sg_output] # replaced previous by variable "sg_id"
   
   tags = {
     Name = "TF EC2 Instance"
