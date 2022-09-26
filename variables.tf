66e2a0c34ce75cd58ffce7c1702f5979b601773d
Prash <prash315@users.noreply.github.com>
2022-09-25 22:27:10 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "ami_id" {
  default     = "ami-0d26eb3972b7f8c96"
  
  validation {
-   condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+   condition     = length(var.ami_id) > 4 && substr(var.ami_id, 0, 4) == "ami-"
    error_message = "Please provide a valid value for variable AMI."
  }
 }
