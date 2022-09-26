e7a64eadcf0248b462b68821b6a18e57a3a1d5f0
Prash <prash315@users.noreply.github.com>
2022-09-25 23:46:48 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,15 @@
 variable "ami_id" {
  type        = string
  description = "AMI ID for the EC2 instance"
- default     = "ami-0d26eb3972b7f8c96"
- 
+ /*
  validation {
    condition     = length(var.ami_id) > 4 && substr(var.ami_id, 0, 4) == "ami-"
    error_message = "Please provide a valid value for variable AMI."
  }
+ */
 }
 
 variable "vpc_id" {
  type        = string
  description = "VPC ID"
- default     = "" 
-
 }
