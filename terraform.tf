bd0534b2a57d60f3a33806c5d0c48d0db83bd319
Prash <prash315@users.noreply.github.com>
2022-09-26 16:31:44 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -12,15 +12,3 @@ terraform {
 }
 
 variable region {}
-
-variable "vpc_id" {
- type        = string
- description = "VPC ID"
- default = ""
-}
-
-variable "ami_id" {
- type        = string
- description = "AMI ID for the EC2 instance"
- default = ""
-}
