43c158b1e2553f4f4efad81fadb937076c38dcf2
Prash <prash315@users.noreply.github.com>
2022-09-25 22:25:32 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+ type        = string
+ description = "AMI ID for the EC2 instance"
+ default     = "ami-0d26eb3972b7f8c96"
+ 
+ validation {
+   condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+   error_message = "Please provide a valid value for variable AMI."
+ }
+}
+
+variable "vpc_id" {
+ type        = string
+ description = "VPC ID"
+ default     = "" 
+
+}
