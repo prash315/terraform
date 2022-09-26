42c628b1c6e5c54be658a53824c604629c9a74d3
Prash <prash315@users.noreply.github.com>
2022-09-25 23:52:40 -0400

    Create ec2vars.tf

---
+++ ec2_module/ec2vars.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {
+ type        = string
+ description = "VPC ID"
+ default = ""
+}
+
+variable "ami_id" {
+ type        = string
+ description = "AMI ID for the EC2 instance"
+ default = ""
+}
