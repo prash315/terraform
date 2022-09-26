6eadddabe9e3fca6760fbae848bdb0fe76559554
Prash <prash315@users.noreply.github.com>
2022-09-25 22:49:00 -0400

    Update terraform.tf

--- ec2_module/terraform.tf
+++ ec2_module/terraform.tf
@@ -4,3 +4,14 @@ variable "vpc_id" {
  default     = "" 
 
 }
+
+variable "ami_id" {
+ type        = string
+ description = "AMI ID for the EC2 instance"
+ default     = "ami-0d26eb3972b7f8c96"
+ 
+ validation {
+   condition     = length(var.ami_id) > 4 && substr(var.ami_id, 0, 4) == "ami-"
+   error_message = "Please provide a valid value for variable AMI."
+ }
+}
