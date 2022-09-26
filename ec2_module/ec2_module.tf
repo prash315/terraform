bf3f18e58dabd63e283bf06d00ff5896c81eb843
Prash <prash315@users.noreply.github.com>
2022-09-26 16:00:33 -0400

    Update ec2_module.tf

--- ec2_module/ec2_module.tf
+++ ec2_module/ec2_module.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform_ec2_instance" {
   ami           = var.ami_id
   instance_type = "t2.micro"
   key_name = "pp_terraform_course"
-  vpc_security_group_ids = ["tf security group"] # replaced previous by variable "sg_id"
+  vpc_security_group_ids = [module.sg_module.sg_output] # replaced previous by variable "sg_id"
   
   tags = {
     Name = "TF EC2 Instance"
